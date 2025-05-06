65f155539e50882f5bcfe9abb67f924ac0eab161
Pouul <paulohuang04@gmail.com>
2025-05-05 21:45:10 -0300

    Arquivos necessarios

--- src/apl2/Node.java
+++ src/apl2/Node.java
@@ -19,82 +19,103 @@ public class Node {
 	// TODO: Implementar a classe conforme o enunciado da atividade Apl2.
     private String id;
     private String nome;
-    private int inteiro;
-    private int decimo;
     private Node next;
     private float nota;
-    private String AnoESementre;
     private Node prev;
-    private String aux;
+    private String Saux;
+    private boolean isnot10;
 
-//    public Node() {
-//        this( -1 , "" , 0 , 0 , null);
-//    }
 
-    public Node(Node prev,String AnoESementre,int id, String nome, int inteiro, int decimo, Node next) {
+    public Node() throws Exception {
+        this(null, -1 , "" , 0 , 0 , null);
+    }
 
-        aux = "23.S1" + String.valueOf(id);
+    public Node(Node prev,int id, String nome, int inteiro, int decimal, Node next) throws Exception {
+        isnot10 = true;
 
-        if(id>000 && id<999){
-            this.id = aux;
+        //variavel auxiliar para juntar o cabeçario e o id
+        Saux = "23.S1-" + String.valueOf(id);
+
+        // filtrando id
+        if(id>0 && id<999){
+            this.id = Saux;
+        }
+        else{
+            throw new Exception("id inválido" + id);
         }
 
         this.nome = nome;
-
+        // quando inteiro ´ou decimal for -1(falta) coloca 99.9 na nota
         if(inteiro == -1 || decimal == -1){
             this.nota = 99.9F;
         }
-        else if (){
-
+        // quando é 10.0
+        else if (inteiro == 10 && decimal == 0){
+            Saux = String.valueOf(inteiro) + String.valueOf(decimal);
+            nota = (float)Integer.parseInt(Saux)/10;
+            isnot10 = false;
         }
-
+        // quando é 0 á 9.9, porque é filtrado por obção acima
+        else if (inteiro >= 0 && inteiro <= 10 && decimal >= 0 && decimal <= 9 && isnot10){
+            Saux = String.valueOf(inteiro) + String.valueOf(decimal);
+            nota = (float)Integer.parseInt(Saux)/10;
+        }
+        else{
+            throw new Exception("dado forá do intervalo dado:" + inteiro + "." + decimal);
+        }
 
         this.next = next;
     }
 
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getNome() {
-//        return nome;
-//    }
-//
-//    public void setNome(String nome) {
-//        this.nome = nome;
-//    }
-//
-//    public int getInteiro() {
-//        return inteiro;
-//    }
-//
-//    public void setInteiro(int inteiro) {
-//        this.inteiro = inteiro;
-//    }
-//
-//    public int getDecimo() {
-//        return decimo;
-//    }
-//
-//    public void setDecimo(int decimo) {
-//        this.decimo = decimo;
-//    }
-//
-//    public NodeOriginal getNext() {
-//        return next;
-//    }
-//
-//    public void setNext(NodeOriginal next) {
-//        this.next = next;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "[dados: (" + id + ";" + nome + ";" + inteiro + ";" + decimo + ") | next: " + next + "]";
-//    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(int id) throws Exception {
+        Saux = "23.S1-" + String.valueOf(id);
+        if(id>0 && id<999){
+            this.id = Saux;
+        }
+        else{
+            throw new Exception("id inválido" + id);
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "[dados: (" + id + ";" + nome + ";" + nota + ") | next: " + next + "]" + "| prev: " + prev + "]";
+    }
 
 }
